6e9d60c9fca7ca07eacec341f8a1528b578b5ea3
adcpm <fabien@bonustrack.co>
2019-02-18 17:27:29 +0700

    Move loading on state

--- src/store/modules/ui.js
+++ src/store/modules/ui.js
@@ -2,17 +2,30 @@ import Vue from 'vue';
 
 const state = {
   sidebarVisible: false,
+  showLoading: false,
 };
 
 const mutations = {
   toggleSidebarVisibility(_state) {
     Vue.set(_state, 'sidebarVisible', !_state.sidebarVisible);
   },
+  showLoading(_state) {
+    Vue.set(_state, 'showLoading', true);
+  },
+  hideLoading(_state) {
+    Vue.set(_state, 'hideLoading', false);
+  },
 };
 
 const actions = {
   toggleSidebarVisibility({ commit }) {
     commit('toggleSidebarVisibility');
+  },
+  showLoading({ commit }) {
+    commit('showLoading');
+  },
+  hideLoading({ commit }) {
+    commit('hideLoading');
   },
 };
 

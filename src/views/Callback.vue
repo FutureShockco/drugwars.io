c7e736318402201b1f9c8f979a40b0b9a1fa9049
adcpm <fabien@bonustrack.co>
2019-02-18 17:28:11 +0700

    Use state loading

--- src/views/Callback.vue
+++ src/views/Callback.vue
@@ -1,6 +1,5 @@
 <template>
   <Center>
-    <VueLoadingIndicator v-if="isLoading" class="overlay fixed big transparent"/>
     <div v-if="!isLoading && error">
       <h1>Oops!</h1>
       <h5>{{ error }}</h5>
@@ -12,21 +11,20 @@
 export default {
   data() {
     return {
-      isLoading: false,
       accessToken: this.$route.query.access_token,
       error: null,
     };
   },
   created() {
-    this.isLoading = true;
+    this.$store.dispatch('showLoading');
     this.$store
       .dispatch('login', this.accessToken)
       .then(() => {
-        this.isLoading = false;
+        this.$store.dispatch('hideLoading');
         this.$router.push('/');
       })
       .catch(() => {
-        this.isLoading = false;
+        this.$store.dispatch('hideLoading');
         this.error = 'Your access token is not valid';
       });
   },

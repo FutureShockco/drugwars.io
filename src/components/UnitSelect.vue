e319a2d3c2d7e0d0898c1cd51def490082803aae
adcpm <fabien@bonustrack.co>
2019-02-19 03:24:15 +0700

    Display units

---
+++ src/components/UnitSelect.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="mb-4 border-bottom pb-4">
+    <h4>{{ unit.unit }}</h4>
+    <input class="input form-control" :value="unit.amount">
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['unit'],
+};
+</script>
--- src/views/Battles.vue
+++ src/views/Battles.vue
@@ -2,7 +2,25 @@
   <div>
     <Header title="Battles" />
     <div class="p-4 after-header">
+      <UnitSelect
+        v-for="unit in units"
+        :unit="unit"
+        :key="unit"
+      />
+      <button class="btn btn-large btn-primary mb-6">
+        Fight
+      </button>
       <p>Battles are disabled temporarily and will come back with a lot of improvements, thank you for your understanding.</p>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  computed: {
+    units() {
+      return this.$store.state.game.user.units;
+    },
+  },
+};
+</script>

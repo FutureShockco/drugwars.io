82363dfbcce038fbe30c26a560a7b38d04274dc2
adcpm <fabien@bonustrack.co>
2019-02-19 07:01:11 +0700

    Use config vars

--- src/helpers/steemconnect.js
+++ src/helpers/steemconnect.js
@@ -1,9 +1,8 @@
 import steemconnect from 'steemconnect';
 
 const client = steemconnect.Initialize({
-  app: 'drugwars.app',
-  // callbackURL: 'https://testnet.drugwars.io/callback',
-  callbackURL: 'http://localhost:8080/callback',
+  app: process.env.VUE_APP_SC_CLIENT_ID,
+  callbackURL: process.env.VUE_APP_SC_REDIRECT_URI,
 });
 
 export default client;

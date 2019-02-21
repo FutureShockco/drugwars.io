33c1141e414648999a91a0ab503f33adb15ab16b
hightouch <hightouch@hotmail.fr>
2019-02-21 02:33:17 +0100

    dashboard

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,7 +2,7 @@
   <div>
     <Header title="Overview" />
     <div class="p-4 after-header">
-      <div class="item p-2">
+      <div class="mb-2 item p-2">
         <h5>DRUGS PRODUCTION <Icon class="ml-2" :size="18" :color="'#fbbd08'" icon="drugs"/> </h5>
       </div>
       <BuildingProductionCompact
@@ -10,11 +10,16 @@
         :building="building"
         :key="building.id"
       />
-      <div class="item mb-4">
-      <h5>Total capacity : {{ drugStorage | amount }}
-        Safe : {{ drugStorage /10 | amount }}</h5>
-      </div>
-      <div class="item p-2">
+      <div class="item mb-4 p-2">
+        <div class="right floated">
+          <h5> Total: {{ user.drug_production_rate * 60 * 60 * 24 | amount }}/Day</h5>
+          <h5> Full in : {{ (drugStorage - user.drugs_balance) / user.drug_production_rate * 1000 | ms}}</h5>
+        </div>
+      <h5>Total capacity : {{ drugStorage | amount }}</h5>
+        <h5 class="text-green">Safe : {{ drugStorage /10 | amount }}
+      </h5>
+        </div>
+      <div class="mb-2 item p-2">
         <h5>WEAPONS PRODUCTION <Icon class="ml-2" :size="18" :color="'#fbbd08'" icon="weapons"/> </h5>
       </div>
       <BuildingProductionCompact
@@ -22,11 +27,15 @@
         :building="building"
         :key="building.id"
       />
-      <div class="item mb-4">
-        <h5>Total capacity : {{ weaponStorage | amount }}
-        Safe : {{ weaponStorage /10 | amount }}</h5>
+      <div class="item mb-4 p-2">
+        <div class="right floated">
+        <h5> Total: {{ user.weapon_production_rate * 60 * 60 * 24 | amount }}/Day</h5>
+          <h5> Full in : {{ (weaponStorage - user.weapons_balance) / user.weapon_production_rate * 1000 | ms }}</h5>
+        </div>
+        <h5>Total capacity : {{ weaponStorage | amount }}</h5>
+          <h5 class="text-green">Safe : {{ weaponStorage /10 | amount }}</h5>
       </div>
-      <div class="item p-2">
+      <div class="mb-2 item p-2">
         <h5>ALCOHOL PRODUCTION <Icon class="ml-2" :size="18" :color="'#fbbd08'" icon="alcohol"/> </h5>
       </div>
         <BuildingProductionCompact
@@ -34,9 +43,13 @@
         :building="building"
         :key="building.id"
       />
-      <div class="item">
-        <h5> Total capacity : {{ alcoholStorage | amount }}
-        Safe : {{ alcoholStorage /10 | amount }}</h5>
+      <div class="item p-2">
+        <div class="right floated">
+        <h5> Total: {{ user.alcohol_production_rate * 60 * 60 * 24 | amount }}/Day</h5>
+          <h5> Full in : {{ (alcoholStorage - user.alcohols_balance) / user.alcohol_production_rate * 1000 | ms}}</h5>
+        </div>
+        <h5> Total capacity : {{ alcoholStorage | amount }}</h5>
+          <h5 class="text-green">Safe : {{ alcoholStorage /10 | amount }}</h5>
       </div>
     </div>
   </div>
@@ -55,6 +68,9 @@ export default {
     };
   },
   computed: {
+    user() {
+      return this.$store.state.game.user.user;
+    },
     drugStorage() {
       return this.$store.state.game.user.user.drug_storage;
     },

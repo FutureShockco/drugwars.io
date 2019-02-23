6907c5b88b3fc3e2a65c04e63f1ae79aa657e79d
adcpm <fabien@bonustrack.co>
2019-02-24 01:19:15 +0700

    Fix link to units

--- src/components/Bootcamp/Tabs.vue
+++ src/components/Bootcamp/Tabs.vue
@@ -1,8 +1,8 @@
 <template>
   <Header>
     <UiTabs>
-      <UiTab to="camp">Units</UiTab>
-      <UiTab to="trainings">Trainings</UiTab>
+      <UiTab to="/units">Units</UiTab>
+      <UiTab to="/trainings">Trainings</UiTab>
     </UiTabs>
   </Header>
 </template>

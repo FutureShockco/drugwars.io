df8dc64670af46d6bc9655de50cef5e6b1d7b98c
adcpm <fabien@bonustrack.co>
2019-03-05 02:15:22 +0700

    Login with WS

--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -1,5 +1,5 @@
 import Vue from 'vue';
-import sc from '@/helpers/steemconnect';
+import client from '@/helpers/client';
 
 const state = {
   username: null,
@@ -22,16 +22,13 @@ const actions = {
   login: async ({ commit }, accessToken = localStorage.getItem('drugwars_token')) =>
     new Promise(resolve => {
       if (accessToken) {
-        sc.setAccessToken(accessToken);
-        sc.me((err, result) => {
-          if (err || !result) {
-            resolve();
-          } else {
-            localStorage.setItem('drugwars_token', accessToken);
-            commit('saveUsername', result.name);
-            commit('saveAccount', result.account);
-            resolve();
-          }
+        client.requestAsync('login', accessToken).then(result => {
+          localStorage.setItem('drugwars_token', accessToken);
+          commit('saveUsername', result.name);
+          commit('saveAccount', result.account);
+          resolve();
+        }).catch(e => {
+          resolve();
         });
       } else {
         resolve();

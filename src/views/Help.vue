5fba6d6984c69d9e4a683b0cb185566d70fce409
adcpm <fabien@bonustrack.co>
2019-03-05 20:39:29 +0700

    Init FAQ

--- src/views/Help.vue
+++ src/views/Help.vue
@@ -23,9 +23,46 @@
           timcliff
         </router-link>
       </p>
+      <h3>F.A.Q.</h3>
+      <input
+        class="input btn-large btn-block mb-4"
+        placeholder="Search"
+        v-model="q"
+      />
+      <div>
+        <div>
+          <div
+            :key="key"
+            v-for="(question, key) in faq"
+            class="mb-4"
+          >
+            <p><b class="mr-3">{{ question.q }}</b></p>
+            <p>{{ question.a }}</p>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
+
+<script>
+import faq from '@/../faq.json';
+
+export default {
+  data() {
+    return {
+      q: '',
+    };
+  },
+  computed: {
+    faq() {
+      return faq.filter((q) => {
+        return JSON.stringify(q).toLowerCase().includes(this.q.toLowerCase());
+      });
+    },
+  },
+};
+</script>
 
 <style scoped class="less">
 .help {

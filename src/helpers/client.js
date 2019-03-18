1cf1e2de02dc2c79349001a6af65e060d28983b2
adcpm <fabien@bonustrack.co>
2019-03-18 23:00:00 +0700

    Use config var for WS API url

--- src/helpers/client.js
+++ src/helpers/client.js
@@ -2,6 +2,6 @@ import Promise from 'bluebird';
 import { Client } from 'drugwars';
 
 Promise.promisifyAll(Client.prototype);
-const client = new Client('wss://api.drugwars.io');
+const client = new Client(process.env.VUE_APP_WS_API_URL);
 
 export default client;

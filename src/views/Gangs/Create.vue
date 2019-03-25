3b23ee756077ed8dfa37ed4269c4d70d7d80d2d1
hightouch <hightouch@hotmail.fr>
2019-03-25 18:29:12 +0100

    fix gang creation for edge and brave

--- src/views/Gangs/Create.vue
+++ src/views/Gangs/Create.vue
@@ -1,44 +1,22 @@
 <template>
-  <div>
-    <GangsTabs />
-    <div class="p-4 text-center">
-      <h2>Create your gang</h2>
-      <Cost
-        class="mx-auto"
-        :drugsCost="gangCreationFee.drugs"
-        :weaponsCost="gangCreationFee.weapons"
-        :alcoholsCost="gangCreationFee.alcohols"
-        :quantity="1"
-      />
-      <form v-if="hasEnough" class="form container-xxs" @submit.prevent="handleSubmit">
-          <p>Id</p>
-          <input
-            class="input input-primary mb-2"
-            v-model="gang"
-            maxlength="16"
-            placeholder="The id of your gang, example: 'frenchconnection'"
-            v-lowercase
-          />
-          <p>Tag</p>
-          <input
-            class="input input-primary mb-4"
-            v-model="ticker"
-            maxlength="6"
-            placeholder="The ticker of you gang, example: 'FRCON'"
-            v-uppercase
-          />
-          <button
-            class="button input-block button-large button-green mb-2"
-            type="submit"
-            :disabled="isLoading"
-          >
-            <Loading v-if="isLoading"/>
-            <span v-else>Create</span>
-          </button>
-      </form>
-      <p v-else>You need more resources to create your gang.</p>
+    <div>
+        <GangsTabs />
+        <div class="p-4 text-center">
+            <h2>Create your gang</h2>
+            <Cost class="mx-auto" :drugsCost="gangCreationFee.drugs" :weaponsCost="gangCreationFee.weapons" :alcoholsCost="gangCreationFee.alcohols" :quantity="1" />
+            <form v-if="hasEnough" class="form container-xxs" @submit.prevent="handleSubmit">
+                <p>Id</p>
+                <input class="input input-primary mb-2" v-model="gang" maxlength="16" placeholder="The id of your gang, example: 'frenchconnection'" v-lowercase />
+                <p>Tag</p>
+                <input class="input input-primary mb-4" v-model="ticker" maxlength="6" placeholder="The ticker of you gang, example: 'FRCON'" v-uppercase />
+                <button class="button input-block button-large button-green mb-2" type="submit" :disabled="isLoading">
+                    <Loading v-if="isLoading"/>
+                    <span v-else>Create</span>
+                  </button>
+            </form>
+            <p v-else>You need more resources to create your gang.</p>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
@@ -83,7 +61,7 @@ export default {
       this.isLoading = true;
 
       const payload = {
-        gang: this.gang.tr(),
+        gang: this.gang.trim().toLowerCase(),
         ticker: this.ticker.toUpperCase(),
       };
 

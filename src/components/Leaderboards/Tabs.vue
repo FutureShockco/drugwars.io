67c0ff6ff3ddfc4c3fd288dad2a58bf24fe3589e
hightouch <hightouch@hotmail.fr>
2019-04-02 14:26:00 +0000

    update heistboard

--- src/components/Leaderboards/Tabs.vue
+++ src/components/Leaderboards/Tabs.vue
@@ -2,6 +2,7 @@
   <Header>
     <UiTabs>
       <UiTab to="/leaderboards">Drug Production</UiTab>
+      <UiTab to="/leaderboards/heistboard">Heist Deposit</UiTab>
     </UiTabs>
   </Header>
 </template>
---
+++ src/views/Leaderboards/Heistboard.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <LeaderboardsTabs/>
+    <div class="p-4" v-if="isLoading || users.length === 0">
+      <Loading/>
+    </div>
+    <div class="mb-4" v-else>
+      <Player
+        v-for="(user, key) in users"
+        :player="user"
+        :key="user.username"
+        :rank="key + 1"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import client from '@/helpers/client';
+
+export default {
+  data() {
+    return {
+      isLoading: false,
+      users: [],
+    };
+  },
+  created() {
+    this.isLoading = true;
+    client.requestAsync('get_heistboard', null).then(result => {
+      this.users = result.players;
+      this.isLoading = false;
+    });
+  },
+};
+</script>

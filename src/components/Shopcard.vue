c027b48e157e06b57cf2074f510747a2c297d009
hightouch <hightouch@hotmail.fr>
2019-05-15 23:50:00 +0000

    fix paypal

--- src/components/Shopcard.vue
+++ src/components/Shopcard.vue
@@ -28,7 +28,7 @@
           :client="credentials"
           :items="myItems"
           env="production"
-          :invoice-number="'1234'+username"
+          :invoice-number="uniqueId+username"
           v-on:payment-authorized="paymentAuthorized"
           v-on:payment-completed="paymentCompleted"
           v-on:payment-cancelled="paymentCancelled"
@@ -77,6 +77,12 @@ export default {
     username() {
       return this.$store.state.auth.username;
     },
+    uniqueId(){
+        return 'xxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+      });
+    }
   },
   components: {
     PayPal,

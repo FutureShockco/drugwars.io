4ebd805a2e4ec2ed92e8bbaf9859b6e9e647a9a2
hightouch <hightouch@hotmail.fr>
2019-08-30 20:57:37 +0000

    fix

--- src/views/Rewards.vue
+++ src/views/Rewards.vue
@@ -4,9 +4,12 @@
         <Prize />
         <div class="p-4 text-center">
             <p>Invest into the bank heist, and convert your DRUGS to STEEM with a daily payment.</p>
-            <div class="container-xxs">
-                <Heist/>
-            </div>
+        <div class="px-4 text-center">
+            <Heist/>
+        </div>
+        <div class="px-4 text-center">
+            <Lottery/>
+        </div>
         </div>
     </div>
 </template>

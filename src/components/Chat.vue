3e4c0e43388ff146fde115adde33d6e9cd4830b4
hightouch67 <cetinkaya.ozden@gmail.com>
2019-09-06 00:19:08 +0000

    f

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -186,6 +186,7 @@ export default {
     socket.on('update-users', users => {
       self.members = users;
     });
+    this.$parent.$data.newmessage = false;
     setTimeout(() => {
       this.scrollToEnd();
     }, 500);

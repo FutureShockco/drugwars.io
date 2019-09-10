b8096a92c77f5c8004d2a1b22535e4790871b4b1
hightouch67 <cetinkaya.ozden@gmail.com>
2019-09-10 17:58:18 +0000

    fix token page

--- src/components/DrugDetail.vue
+++ src/components/DrugDetail.vue
@@ -1,14 +1,73 @@
 <template>
     <div>
-        <div class="d-flex flex-lg-row text-center text-lg-left item">
-            <div class="mr-3">
+        <div class="text-center mb-6 item border-bottom">
+				<div class="text-center p-4">
+					<h5 class="columns">DW-{{drug.name.substring(0,4)}} Token Informations</h5>
+					<h6 class="column col-3 m-0">
+						Max Supply
+						<p class="text-yellow"> 0</p>
+					</h6>
+					<h6 class="column col-3 m-0 border-left">
+						Burn pending
+						<p class="text-yellow"> 0</p>
+					</h6>
+					<h6 class="column col-3 m-0 border-left">
+						Burnt
+						<p class="text-yellow">0</p>
+					</h6>
+          <h6 class="column col-3 m-0 border-left">
+						Staking
+						<p class="text-yellow">None</p>
+					</h6>
+					<h5 class="columns">
+						Token distribution
+					</h5>
+					<h6 class="column col-4 m-0 ">
+						Total circulating
+						<p class="text-yellow">0</p>
+					</h6>
+					<h6 class="column col-4 m-0 border-left">
+						Circulating (ON MARKET)
+						<p class="text-yellow">0</p>
+					</h6>
+					<h6 class="column col-4 m-0 border-left">
+						Circulating (IN GAME)
+						<p class="text-yellow">0</p>
+					</h6>
+					<h5 class="columns">Statistics</h5>
+					<h6 class="column col-3 m-0">
+						Volume (24h)
+						<p class="text-yellow">0</p>
+					</h6>
+					<h6 class="column col-3 m-0 border-left">
+						Price Change
+						<p class="text-red">0%</p>
+					</h6>
+					<h6 class="column col-3 m-0 border-left">
+						Last Price
+						<p> $0 </p>
+					</h6>
+					<h6 class="column col-3 m-0 border-left">
+						Bid/Ask
+						<p> $0</p>
+					</h6>
+				</div>
+          <div class="mt-6">
                 <img class="preview" :src="`/img/icons/drugs/${drug.icon}`">
             </div> 
             <div class="item-content width-full">
                 <h5>{{ drug.name }}</h5>
                 <div class="mb-2" v-html="drug.description"></div>
+                <button disabled
+                  class="button btn-block button-blue mb-2">
+                  <i class="iconfont icon-zap"/>
+                TRADE ON MARKET
+                </button>
             </div> 
-        </div>
+          </div> 
+          <div class="px-4">
+            DRUGWARS DOLLARS (DWD) OR DRUGWARS {{drug.name.toUpperCase()}} (DW-{{drug.name.substring(0,4).toUpperCase()}}) TOKENS ARE VIRTUAL CURRENCIES. 
+          </div>
     </div>
 </template>
 

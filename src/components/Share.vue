d8c1386a135a1c16ae8573b9d1cc2250d8266ba5
hightouch67 <cetinkaya.ozden@gmail.com>
2019-09-30 04:34:20 +0000

    test share

--- src/components/Share.vue
+++ src/components/Share.vue
@@ -46,7 +46,8 @@ export default {
       const xhr = new XMLHttpRequest();
       setTimeout(() => {
         const node = document.getElementById(this.fight_key.slice(0, 10));
-        const sharenode = node.removeChild(node.childNodes[1])
+        const sharenode = node;
+         sharenode.removeChild(sharenode.childNodes[1])
         domtoimage.toJpeg(sharenode, { quality: 1 }).then(dataUrl => {
           const url = `https://api.cloudinary.com/v1_1/${cloudName}/upload`;
           const fd = new FormData();

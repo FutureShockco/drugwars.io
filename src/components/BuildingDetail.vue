d7c9d64a21488bf9d35ec954f6e3df892fb23e3d
hightouch67 <cetinkaya.ozden@gmail.com>
2019-10-07 09:53:04 +0000

    add details for biuldings

--- src/components/BuildingDetail.vue
+++ src/components/BuildingDetail.vue
@@ -26,13 +26,73 @@
                     <div v-else><b>Safe:</b> {{ 10000 /100*25 | amount }}</div>
                 </div>
                 <div v-if="['headquarters'].includes(building.id)" class="mb-2">
-                    Coming Soon
+                  <div class="columns">
+     <div class="column col-1">
+    <div v-if="level"><b>Lvl 10</b> </div>
+                    <div v-if="level"><b>Lvl 20</b> </div>
+                    <div v-if="level"><b>Lvl 50</b> </div>
+                    <div v-if="level"><b>Lvl 100</b></div>
+                    
+                  </div>
+                   <div class="column col-1">
+                     Building
+                  Timer
+                  </div>
+                  <div class="column col-2">
+    <div v-if="level"><b>Training Fac.</b> {{updateTime('training_facility',level,10) | ms}}</div>
+                    <div v-if="level"><b>Training Fac.</b> {{updateTime('training_facility',level,20) | ms}}</div>
+                    <div v-if="level"><b>Training Fac.</b> {{updateTime('training_facility',level,50) | ms}}</div>
+                    <div v-if="level"><b>Training Fac.</b> {{updateTime('training_facility',level,100) | ms}}</div>
+                    
+                  </div>
+                                                        <div class="column col-2">
+                    <div v-if="level"><b>Crack House</b> {{updateTime('crackhouse',level,10) | ms}}</div>
+                    <div v-if="level"><b>Crack House</b> {{updateTime('crackhouse',level,20) | ms}}</div>
+                    <div v-if="level"><b>Crack House</b> {{updateTime('crackhouse',level,50) | ms}}</div>
+                    <div v-if="level"><b>Crack House</b> {{updateTime('crackhouse',level,100) | ms}}</div>
+                    
+                  </div>
+                  <div class="column col-2">
+      <div v-if="level"><b>Pharma. Lab</b> {{updateTime('pharma_lab',level,10) | ms}}</div>
+                    <div v-if="level"><b>Pharma. Lab</b> {{updateTime('pharma_lab',level,20) | ms}}</div>
+                    <div v-if="level"><b>Pharma. Lab</b> {{updateTime('pharma_lab',level,50) | ms}}</div>
+                    <div v-if="level"><b>Pharma. Lab</b> {{updateTime('pharma_lab',level,100) | ms}}</div>
+                    
+                  </div>
+                  <div class="column col-2">
+    <div v-if="level"><b>Research C.</b> {{updateTime('research_center',level,10) | ms}}</div>
+                    <div v-if="level"><b>Research C.</b> {{updateTime('research_center',level,20) | ms}}</div>
+                    <div v-if="level"><b>Research C.</b> {{updateTime('research_center',level,50) | ms}}</div>
+                    <div v-if="level"><b>Research C.</b> {{updateTime('research_center',level,100) | ms}}</div>
+                    
+                  </div>
+                    <div class="column col-2">
+                    <div v-if="level"><b>Drug Storage</b> {{updateTime('drug_storage',level,10) | ms}}</div>
+                    <div v-if="level"><b>Drug Storage</b> {{updateTime('drug_storage',level,20) | ms}}</div>
+                    <div v-if="level"><b>Drug Storage</b> {{updateTime('drug_storage',level,50) | ms}}</div>
+                    <div v-if="level"><b>Drug Storage</b> {{updateTime('drug_storage',level,100) | ms}}</div>
+                    
+                  </div>
+
+                  </div>
                 </div>
                 <div v-if="['training_facility'].includes(building.id)" class="mb-2">
                     <div v-if="level"><b>Super Bouncer:</b> {{(0.84 * 1500/ (level)) * ( 1000) | ms}}</div>
                     <div v-if="level"><b>Big Mama:</b> {{(4.72 * 1500/ level ) * (1000) | ms}}</div>
                     <div v-if="level"><b>Spy :</b> {{(0.65 * 1500/ level ) * (1000) | ms}}</div>
                     <div v-if="level"><b>Mercenary:</b> {{(8 * 1500/ level)* (1000) | ms}}</div>
+                </div>
+                 <div v-if="['operation_center'].includes(building.id)" class="mb-2">
+                    <div v-if="level"><b>Drug Bonus</b> +{{drugBonus(level) ||amount}} per day</div>
+                    <div v-if="level"><b>Weapon Bonus</b> +{{weaponBonus(level) ||amount}} per day</div>
+                    <div v-if="level"><b>Alcohol Bonus</b> +{{alcoholBonus(level) ||amount}} per day</div>
+                </div>
+                <div v-if="['research_center'].includes(building.id)" class="mb-2">
+                   <div v-if="level"><b>Routing</b> {{updateTimeTrainings('routing',level,10) | ms}}</div>
+                    <div v-if="level"><b>Close Combat</b> {{updateTimeTrainings('closecombat',level,20) | ms}}</div>
+                    <div v-if="level"><b>FireArms</b> {{updateTimeTrainings('firearms',level,50) | ms}}</div>
+                    <div v-if="level"><b>Protection</b> {{updateTimeTrainings('protection',level,100) | ms}}</div>
+                     <div v-if="level"><b>Psychological Training</b> {{updateTimeTrainings('psychological',level,100) | ms}}</div>
                 </div>
             </div>
         </div>
@@ -41,7 +101,7 @@
 </template>
 
 <script>
-import { buildings, utils } from 'drugwars';
+import { buildings, utils,trainings } from 'drugwars';
 import { getBalances } from '@/helpers/utils';
 import { pickBy } from 'lodash';
 
@@ -172,7 +232,6 @@ export default {
             b.territory === this.$store.state.game.mainbase.territory,
         );
       }
-
       return (
         this.$store.state.game.user.buildings.find(b => b.building === 'headquarters') || { lvl: 0 }
       );
@@ -187,6 +246,28 @@ export default {
     },
     alcoholsCost(lvl) {
       return utils.calculateCostToUpgrade(this.building.alcohols_cost, lvl);
+    },
+    updateTime(id,level,blevel) {
+      return utils.calculateTimeToBuild(id, buildings[id].coeff, blevel, level);
+    },
+    updateTimeTrainings(id,level,blevel) {
+      return utils.calculateTimeToBuild(id, trainings[id].coeff, blevel, level);
+    },
+    drugBonus(lvl) {
+      let oc = lvl;
+      let labLvl = 0;
+      return parseFloat(this.HQ.drug_production_rate * 60 * 60 * 24 * oc * 0.005 ).toFixed(2);
+    },
+    weaponBonus(lvl) {
+      let oc = lvl;
+      return parseFloat(
+        this.HQ.weapon_production_rate * 60 * 60 * 24 * oc * 0.005).toFixed(2);
+    },
+    alcoholBonus(lvl) {
+      let oc = lvl;
+      return parseFloat(
+        this.HQ.alcohol_production_rate * 60 * 60 * 24 * oc * 0.005
+      ).toFixed(2);
     },
   },
 };

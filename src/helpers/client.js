99ebd87f19fea59bbf6cf8379833c3b5feb37b1d
hightouch67 <cetinkaya.ozden@gmail.com>
2019-10-08 04:50:06 +0000

    refresh at end of station

--- src/helpers/client.js
+++ src/helpers/client.js
@@ -125,6 +125,17 @@ function Sub(rawClient) {
       });
     }
 
+    if (message[1].body === 'end_station') {
+      store.dispatch('refresh_sent_fights_count');
+      store.dispatch('refresh_sent_transport_count');
+      store.dispatch('refresh_sent_station_count');
+      store.dispatch('init');
+      store.dispatch('notify', {
+        type: 'success',
+        message: 'Your troops have reached their station destination!',
+      });
+    }
+    
     if (message[1].body === 'end_inc_attack') {
       store.dispatch('refresh_inc_fights_count');
       store.dispatch('init');
